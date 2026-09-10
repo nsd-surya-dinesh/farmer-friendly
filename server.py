e41e89220ddce0dda0d6e56ee68fbead030aa028
Naga Surya Dinesh Ganta <nagasuryadinesh@gmail.com>
2026-09-10 15:40:06 +0530

    Update server.py

--- server.py
+++ server.py
@@ -21,7 +21,7 @@ from datetime import datetime
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 from PIL import Image
-import google.genai as genai
+import google.generativeai as genai
 from dotenv import load_dotenv
 
 load_dotenv()
